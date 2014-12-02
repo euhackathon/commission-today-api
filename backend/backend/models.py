1d7ddf9e915b062529ea6d921204bcc123684c3d
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-12-03 00:44:23 +0100

    Add organizations #18

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -17,10 +17,19 @@ class Member(models.Model):
     def __unicode__(self):
         return self.name
 
+class Organization(models.Model):
+    name = models.CharField(max_length=128)
+    money = models.IntegerField()
+    lobbyists = models.IntegerField()
+    lobbyists_with_access = models.IntegerField()
+    explore_url = models.CharField(max_length=128)
+
 class Meeting(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=512)
     member = models.ForeignKey(Member)
     lobby = models.BooleanField(default=False)
+    # TODO: @palcu make a many to many relationship
+    organization = models.ForeignKey(Organization, null=True)
     def __unicode__(self):
         return self.description
