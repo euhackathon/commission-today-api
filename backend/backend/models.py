37db399d510484c5fbcd8542f827ab679df06b28
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-12-02 21:32:58 +0100

    Return a nice string #14

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Portofolio(models.Model):
     name = models.CharField(max_length=128)
+    def __str__(self):
+        return self.name
 
 class Member(models.Model):
     name = models.CharField(max_length=128)
@@ -9,9 +11,13 @@ class Member(models.Model):
     url = models.CharField(max_length=512)
     photoUrl = models.CharField(max_length=512)
     portofolio = models.ForeignKey(Portofolio)
+    def __str__(self):
+        return self.name
 
 class Meeting(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=512)
     member = models.ForeignKey(Member)
     lobby = models.BooleanField(default=False)
+    def __str__(self):
+        return self.description
