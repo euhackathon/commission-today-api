8d2aa6bb256b2e63e9052d4be8773c14cc00e61b
Alex Palcuie <alex.palcuie@hootsuite.com>
2014-12-03 00:47:40 +0100

    Add endpoints for organization #18

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,18 +2,21 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from tastypie.api import Api
-from backend.models import Portofolio, Member, Meeting
-from api.resources import PortofolioResource, MemberResource, MeetingResource
+from backend.models import Portofolio, Member, Meeting, Organization
+from api.resources import (PortofolioResource, MemberResource, MeetingResource,
+                           OrganizationResource)
 
 
 v1_api = Api(api_name='v1')
 v1_api.register(PortofolioResource())
 v1_api.register(MemberResource())
 v1_api.register(MeetingResource())
+v1_api.register(OrganizationResource())
 
 admin.site.register(Portofolio)
 admin.site.register(Member)
 admin.site.register(Meeting)
+admin.site.register(Organization)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
